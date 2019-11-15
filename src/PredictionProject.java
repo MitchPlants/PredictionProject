a58847b26b688a11376ff58f1b4f52ec422c93ed
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-11-15 09:17:54 -0500

    # of days in the month method

--- src/PredictionProject.java
+++ src/PredictionProject.java
@@ -10,8 +10,17 @@ public class PredictionProject {
         int h, q, m, k, j, year;
         String Day;
         Scanner Keyboard;
-        
+        String Month;
+        int NumberOfDays;
         Keyboard = new Scanner(System.in);
+        System.out.println("Monday's child is fair of face,\n" +
+                "Tuesday's child is full of grace,\n" +
+                "Wednesday's child is full of woe,\n" +
+                "Thursday's child has far to go.\n" +
+                "Friday's child is loving and giving,\n" +
+                "Saturday's child works hard for a living,\n" +
+                "But the child born on the Sabbath Day,\n" +
+                "Is fair and wise and good in every way.\n");
 
         System.out.println("Day of the month?");
         q = Keyboard.nextInt();
@@ -26,17 +35,86 @@ public class PredictionProject {
 
         Day = days(h);
         System.out.println(m + "/" + q + "/" + year + " is a " + Day);
+        System.out.println(MonthDays(m,year));
+        
 
-        switch(m){
+    }
+    public static String MonthDays(int m,int year) {
+        String Month;
+        String NumberOfDays;
+        switch (m) {
             case 1:
+            case 13:
+                Month = "January has 31 days";
+                break;
+            case 2:
+            case 14:
+                if (year % 4 == 0) {
+                    if (year % 100 == 0) {
+                        Month = "February has 28 days";
+                    } else if (!(year % 100 == 0)) {
+                        if (year % 400 == 0) {
+                            Month = "February has 29 daus";
+                        } else {
+                            Month = "February has 29 days";
+                        }
+                    } else {
+                        Month = "February has 29 days";
+                    }
 
+                } else {
+                    Month = "February has 28 days";
+                }
+                break;
+            case 3:
+                Month = "March has 31 days";
+                break;
+            case 4:
+                Month = "April has 30 days";
+                break;
+            case 5:
+                Month = "May has 31 days";
 
+                break;
+            case 6:
+                Month = "June has 30 days";
 
+                break;
+            case 7:
+                Month = "July has 31 days";
 
+                break;
+            case 8:
+                Month = "August has 31 days";
+
+                break;
+            case 9:
+                Month = "September has 30 days";
+
+                break;
+            case 10:
+                Month = "October has 31 days";
+
+                break;
+            case 11:
+                Month = "November has 30 days";
+
+                break;
+            case 12:
+                Month = "December has 31 days";
+
+                break;
+            default:
+                Month = "Invalid Month has 0 days try again.";
 
         }
 
+        return Month;
+
+
     }
+
+
 
 
 // # that's on . luv ;D XDDD swag what's up homie watch we whip watch me nae neighhhhh im a horse what it do baybeeeeeee -Moira Taylor
